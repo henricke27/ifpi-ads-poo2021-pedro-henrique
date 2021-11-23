01296c0c9514dfdddf45eafc1b715ba5681f6025
henricke27 <84256973+henricke27@users.noreply.github.com>
2021-11-23 10:47:22 -0300

    Update app.ts

--- poo_exercicio_04_pt1/app.ts
+++ poo_exercicio_04_pt1/app.ts
@@ -2,21 +2,21 @@
 let numeros : Array<number> = [1, 2, 3, 4, 5];
 let somatorio : number = 0;
 
-numeros.forEach(number => somatorio += number)
-console.log(somatorio)
+numeros.forEach(number => somatorio += number);
+console.log(somatorio);
 
 /*Segunda questão: O código irá exibir todos os valores contidos no array de number. Também será exibido um valor
 undefined no final, devido a condição "<=" do laço;*/
 
 //Terceira questão;
-let numbers : Array<number> = [7,1,9,3,4,8,6,2,10]
-console.log("---------- Ordem crescente ----------")
-numbers.sort((a, b) => a - b)
-numbers.forEach(number => console.log(number))
+let numbers : Array<number> = [7,1,9,3,4,8,6,2,10];
+console.log("---------- Ordem crescente ----------");
+numbers.sort((a, b) => a - b);
+numbers.forEach(number => console.log(number));
 
-console.log("---------- Ordem decrescente ----------")
-numbers.sort((a, b) => b - a)
-numbers.forEach(number => console.log(number))
+console.log("---------- Ordem decrescente ----------");
+numbers.sort((a, b) => b - a);
+numbers.forEach(number => console.log(number));
 
 //Quarta questão;
 let texto : string = "Instituto Federal do Piauí";
